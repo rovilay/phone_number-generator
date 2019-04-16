62e0fce0354fdef63c21d38e9aa75d940a015f1c
rovilay <rovilayjnr01@gmail.com>
2019-04-16 11:36:15 +0100

    ch-setup-server
    * setup routes
    * add error handler middleware
    * setup cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+/* eslint no-console: 0 */
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
+import routes from './routes';
 
 dotenv.config();
 
 const { PORT } = process.env;
 
 const app = express();
+
+app.use(cors());
+// init routes
+routes(app);
 
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
